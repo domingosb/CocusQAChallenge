1a0feeef214975fd8f152ec511f7a45242d36ffe
domingosb <domingosb95@gmail.com>
2023-08-02 15:12:55 +0100

    Run tests in terminal.

--- src/test/java/org/domingosb/CocusQAChallenge/testRunner.java
+++ src/test/java/org/domingosb/CocusQAChallenge/testRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/features/SwipeTest.feature",
+		features = "src/test/resources/features",
 		plugin = { "pretty", "html:target/cucumber-reports/report.html" },
 		monochrome = true
 	)
